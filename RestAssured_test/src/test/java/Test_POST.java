33f5523a1d9e5bd378e4c3bb15b1e5c174b5ff3a
Guilherme Carini <Carinigcontact@gmail.com>
2021-09-05 23:25:56 -0300

    POST_PUT_PATCH_DELETE

--- RestAssured_test/src/test/java/Test_POST.java
+++ RestAssured_test/src/test/java/Test_POST.java
@@ -1,6 +1,12 @@
-import java.util.HashMap;
-import  java.util.Map;
+//import java.util.HashMap;
+//import  java.util.Map;
+
+import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
 
 
 
@@ -9,11 +15,86 @@ public class Test_POST {
 	@Test
 	public void test_1_post() {
 		
-		Map<String, Object>  map  = new HashMap<String, Object>();		
+//		Map<String, Object>  map  = new HashMap<String, Object>();		
+//		
+//		map.put("name", "Guilherme");
+//		map.put("job","analista de testes");
+//		
+//		System.out.println((map));
+//		
+		JSONObject request = new JSONObject();
 		
-		map.put("name", "Guilherme");
-		map.put("job","analista de testes");
+		request.put("name", "Guilherme");
+		request.put("job","analista de testes");
 		
-		System.out.println((map));
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Content-Type","application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			post("https://reqres.in/api/users").
+		then().
+			statusCode(201);
+	}
+	@Test
+	public void test_2_put() {
+			
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Guilherme");
+		request.put("job","analista de testes");
+		
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Content-Type","application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			put("https://reqres.in/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+		
+	}
+	@Test
+	public void test_3_patch() {
+		
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Guilherme");
+		request.put("job","analista de testes");
+		
+		System.out.println(request);
+		System.out.println(request.toJSONString());
+		
+		given().
+			header("Content-Type","application/json").
+			contentType(ContentType.JSON).
+			accept(ContentType.JSON).
+			body(request.toJSONString()).
+		when().
+			patch("https://reqres.in/api/users/2").
+		then().
+			statusCode(200).
+			log().all();
+		
+	}
+	@Test
+	public void test_4_delete() {
+		
+		
+		when().
+			delete("https://reqres.in/api/users/2").
+		then().
+			statusCode(204).
+			log().all();
+		
 	}
 }
